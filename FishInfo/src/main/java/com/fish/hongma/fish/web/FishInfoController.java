8fa777cf9c8ccfd10b8a8e93425681bdf3812507
hongma-dev <lovely4750@gmail.com>
2021-07-13 20:18:26 +0900

    response xml 적용

--- FishInfo/src/main/java/com/fish/hongma/fish/web/FishInfoController.java
+++ FishInfo/src/main/java/com/fish/hongma/fish/web/FishInfoController.java
@@ -3,12 +3,15 @@ package com.fish.hongma.fish.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.fish.hongma.fish.service.FishInfoVO;
 import com.fish.hongma.fish.service.impl.FishInfoServiceImpl;
 
@@ -19,7 +22,7 @@ public class FishInfoController {
 	@Autowired
 	FishInfoServiceImpl fishInfoServiceImpl;
 	
-	@GetMapping(path="getFishInfos")
+	@GetMapping(path="/getFishInfos")
 	public @ResponseBody List<FishInfoVO> getFishInfos() {
 		
 		List<FishInfoVO> result = fishInfoServiceImpl.getFishInfos();
